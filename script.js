8922d3a95e510c89ad9eeed6b59572d2a21d5083
Mateus Pereira <118226023+mateuscromado@users.noreply.github.com>
2023-12-19 16:59:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
+
+
+
+
+
+
 const carrossel = document.getElementById("carr")
 var largura = document.querySelector("body").offsetWidth
 var i =0
 var contador = 1
+var movimento = 0
 
 function rodar(){
     i +=1
     var largura = document.querySelector("body").offsetWidth;
-    largura = largura*i
+    movimento = largura*i
 
 
-    if(largura <= 1024 && largura >= 650){
-        contador = 2    
-
+   
+    if(largura <= 1024){
+        contador = 2
+    }
+    if(largura <= 768){
+        contador = 5
+    }
+    if(largura <= 600){
+        contador = 5
+    }
+    if(largura <= 380){
+        contador = 5
     }
 
     
@@ -22,14 +38,28 @@ function rodar(){
         carrossel.style.left = "0px"
         i = 0
     }else{
-        carrossel.style.left = `-${largura}px`
+        carrossel.style.left = `-${movimento}px`
     }
 
     console.log(largura )
 }
+var o = 0 
+var medi =0 
+function voltar(){
+    o+=1
+
+    var largura1 = document.querySelector("body").offsetWidth;
 
 
-console.log(largura)
+    carrossel.style.left = `${largura1}px`
+    if(o > medi ){
+        carrossel.style.left = 0
+    }
+
+}
+
+
+console.log(largura);
 
 
 
