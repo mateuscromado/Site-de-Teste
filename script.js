226bd8943e28c13938a6a04a9019879125e0c682
Mateus Pereira <118226023+mateuscromado@users.noreply.github.com>
2023-12-22 16:56:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,70 +1,61 @@
+const carrossel = document.getElementById("carr");
+var largura = document.querySelector("body").offsetWidth;
+var i = 0;
+var contador = 1;
+var movimento = 0;
 
-
-
-
-
-
-const carrossel = document.getElementById("carr")
-var largura = document.querySelector("body").offsetWidth
-var i =0
-var contador = 1
-var movimento = 0
-
-function rodar(){
-    i +=1
+function rodar() {
+    i += 1;
     var largura = document.querySelector("body").offsetWidth;
-    movimento = largura*i
+    movimento = largura * i;
 
-
-  if(largura == 1025){
-    contador = 0
-  }
-    if(largura <= 1024){
-        contador = 2
+    if (largura == 1025) {
+        contador = 0;
     }
-    if(largura <= 768){
-        contador = 5
+    if (largura <= 1024) {
+        contador = 2;
     }
-    if(largura <= 600){
-        contador = 5
+    if (largura <= 768) {
+        contador = 5;
     }
-    if(largura <= 380){
-        contador = 5
+    if (largura <= 600) {
+        contador = 5;
     }
-
-    
-
-
-
-    if(i>contador){
-        carrossel.style.left = "0px"
-        i = 0
-    }else{
-        carrossel.style.left = `-${movimento}px`
+    if (largura <= 380) {
+        contador = 5;
     }
 
-    console.log(largura )
+    if (i > contador) {
+        carrossel.style.left = "0px";
+        i = 0;
+    } else {
+        carrossel.style.left = `-${movimento}px`;
+    }
+
+    console.log(largura);
 }
-var o = 0 
-var medi =0 
-function voltar(){
-    o+=1
+
+function voltar() {
+    o += 1;
 
     var largura1 = document.querySelector("body").offsetWidth;
 
-
-    carrossel.style.left = `${largura1}px`
-    if(o > medi ){
-        carrossel.style.left = 0
+    carrossel.style.left = `${largura1}px`;
+    if (o > medi) {
+        carrossel.style.left = 0;
     }
-
 }
 
-setInterval(rodar,5000)
+// Adiciona um listener para o evento de clique nos botões do slider
+document.querySelector("#anterior").addEventListener("click", function (event) {
+    event.preventDefault(); // Evita o comportamento padrão do link
+    voltar();
+});
 
+document.querySelector("#proximo").addEventListener("click", function (event) {
+    event.preventDefault(); // Evita o comportamento padrão do link
+    rodar();
+});
 
 
 console.log(largura);
-
-
-
